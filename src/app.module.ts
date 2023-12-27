5a043fa813cd7e6eb4de377960a431e1c81f8267
unknown <naveen1001996y@gmail.com>
2023-12-27 10:33:09 +0530

    backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
-import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LoginModule } from './login/login.module';
 import { CoinModule } from './coin/coin.module';
-
+import { typeOrmConfig } from './config/typeorm.config';
+import { LoginModule } from './login/login.module';
+import { TasksModule } from './tasks/tasks.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
